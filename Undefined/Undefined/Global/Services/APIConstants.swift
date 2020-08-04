a9e97285a279e377534a4829632eb16acb7db8ba
junhyeon <oh.junhyeon95@gmail.com>
2020-08-04 19:21:35 +0900

    [UPDATE] 프로젝트 BaseCode

---
+++ Undefined/Undefined/Global/Services/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConstants.swift
+//  Undefined
+//
+//  Created by 오준현 on 2020/08/04.
+//  Copyright © 2020 Undefined. All rights reserved.
+//
+
+import Foundation
+
+struct APIConstants {
+    
+}
