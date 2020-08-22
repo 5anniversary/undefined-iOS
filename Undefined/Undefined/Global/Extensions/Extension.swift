bb2f9e0eab217578cd334ed7cbfaa7091492c206
junhyeon <oh.junhyeon95@gmail.com>
2020-08-22 18:17:27 +0900

    update: 카드 뷰 연결
    맞지 않는 코딩컨벤션 수정 예정

--- Undefined/Undefined/Global/Extensions/Extension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Extension.swift
-//  Undefined
-//
-//  Created by 오준현 on 2020/08/04.
-//  Copyright © 2020 Undefined. All rights reserved.
-//
-
-import Foundation
---
+++ Undefined/Undefined/Global/Extensions/UIView+.swift
@@ -0,0 +1,29 @@
+//
+//  UIView+.swift
+//  Undefined
+//
+//  Created by 오준현 on 2020/08/21.
+//  Copyright © 2020 Undefined. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func gz_pinToSuperview(with insets: UIEdgeInsets = .zero, edges: UIRectEdge = .all) {
+        guard let superview = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        if edges.contains(.top) {
+            topAnchor.constraint(equalTo: superview.topAnchor, constant: insets.top).isActive = true
+        }
+        if edges.contains(.bottom) {
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -insets.bottom).isActive = true
+        }
+        if edges.contains(.left) {
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: insets.left).isActive = true
+        }
+        if edges.contains(.right) {
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -insets.right).isActive = true
+        }
+    }
+}
+
