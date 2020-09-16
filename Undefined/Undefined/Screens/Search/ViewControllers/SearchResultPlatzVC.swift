4190f61d1150187103cb8a65480f58dd69dbb500
totoroto <lsb0496@gmail.com>
2020-09-16 20:23:25 +0900

    [create] 장소검색, 플라츠검색 뷰컨트롤러 추가

---
+++ Undefined/Undefined/Screens/Search/ViewControllers/SearchResultPlatzVC.swift
@@ -0,0 +1,31 @@
+//
+//  SearchResultVC.swift
+//  Undefined
+//
+//  Created by pandora on 2020/09/02.
+//  Copyright © 2020 Undefined. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultPlatzVC: UIPageViewController {
+    let pageIndex = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
