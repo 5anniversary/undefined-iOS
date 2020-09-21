7c31962955c2fe28ee9adcebdbeccec65f0df6ee
junhyeon <oh.junhyeon95@gmail.com>
2020-09-21 15:33:50 +0900

    create: profleCell 추가

---
+++ Undefined/Undefined/Screens/Profile/Cells/ProfileTVC.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTVC.swift
+//  Undefined
+//
+//  Created by 오준현 on 2020/09/21.
+//  Copyright © 2020 Undefined. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Undefined/Undefined/Screens/Profile/ViewControllers/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  Undefined
+//
+//  Created by 오준현 on 2020/09/21.
+//  Copyright © 2020 Undefined. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
