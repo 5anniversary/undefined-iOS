5903d10e1b7d3ef8b18e40b9c3b242040794f8ed
junhyeon <oh.junhyeon95@gmail.com>
2020-09-21 15:33:59 +0900

    update: color 추가

--- Undefined/Undefined/Global/Extensions/UIColor+.swift
+++ Undefined/Undefined/Global/Extensions/UIColor+.swift
@@ -8,8 +8,20 @@
 
 extension UIColor {
 
+  @nonobjc class var veryLightPink: UIColor {
+    return UIColor(white: 234.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkTwo: UIColor {
+    return UIColor(white: 242.0 / 255.0, alpha: 1.0)
+  }
+
   @nonobjc class var lightishPurple: UIColor {
     return UIColor(red: 162.0 / 255.0, green: 62.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var paleLavender: UIColor {
+    return UIColor(red: 239.0 / 255.0, green: 227.0 / 255.0, blue: 1.0, alpha: 1.0)
   }
 
 }
