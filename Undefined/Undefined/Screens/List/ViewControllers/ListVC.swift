564798d3644d18697a973f6e7c1bacfd521aa032
junhyeon <oh.junhyeon95@gmail.com>
2020-09-29 16:23:39 +0900

    [UPDATE] modified ListVC

--- Undefined/Undefined/Screens/List/ViewControllers/ListVC.swift
+++ Undefined/Undefined/Screens/List/ViewControllers/ListVC.swift
@@ -8,23 +8,100 @@
 
 import UIKit
 
+import Then
+import SnapKit
+
 class ListVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - UI Components
+    
+    let listTableView = UITableView(frame: .zero,
+                                    style: .grouped).then
+        {
+            $0.separatorStyle = .none
+            $0.backgroundColor = .white
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        layout()
+        tableView()
     }
-    */
+    
+}
 
+// MARK: - Helper
+
+extension ListVC {
+    
+    func layout() {
+        view.addSubview(listTableView)
+        
+        listTableView.snp.makeConstraints { (make) in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    func tableView() {
+        listTableView.delegate = self
+        listTableView.dataSource = self
+        listTableView.register(MainListTVC.self, forCellReuseIdentifier: MainListTVC.identifier)
+    }
+    
+}
+
+// MARK: Table View Code
+
+extension ListVC: UITableViewDelegate {
+}
+
+extension ListVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MainListTVC.identifier,
+                                                 for: indexPath) as! MainListTVC
+        
+        cell.setUpLayout()
+        cell.rootVC = self
+        cell.contentView.backgroundColor = .white
+        cell.backgroundColor = .white
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 144
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return 217
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   viewForHeaderInSection section: Int) -> UIView? {
+        
+        let view = ProfileView()
+        
+        view.layout()
+        view.layoutListView()
+        
+        return view
+    }
+    
 }
--- Undefined/Undefined/Screens/Profile/ViewControllers/ProfileView.swift
+++ Undefined/Undefined/Screens/Profile/ViewControllers/ProfileView.swift
@@ -13,7 +13,10 @@ class ProfileView: UIView {
         $0.backgroundColor = .paleLavender
     }
     
-    let profileImageView = UIImageView()
+    let profileImageView = UIImageView().then {
+        $0.backgroundColor = .veryLightPink
+        $0.layer.cornerRadius = 35
+    }
     
     let profileImageButton = UIButton()
     
@@ -26,6 +29,11 @@ class ProfileView: UIView {
     let userGuideLabel = UILabel()
         
     let userSettingButton = UIButton()
+    
+    let tableViewGuideLabel = UILabel().then {
+        $0.text = "플라츠 리스트"
+        $0.font = Font.bold.of(size: 14)
+    }
     
     func layout() {
         addSubview(profileBackgroundButton)
@@ -50,7 +58,21 @@ class ProfileView: UIView {
             make.width.equalTo(70)
             make.height.equalTo(70)
         }
-        
+
+    }
+    
+    func layoutProfileView() {
         
     }
+    
+    func layoutListView() {
+        addSubview(tableViewGuideLabel)
+
+        tableViewGuideLabel.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(18)
+            make.top.equalToSuperview().offset(179)
+        }
+
+    }
+    
 }
